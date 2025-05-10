3b4cbf292f56fd80606d053aa1971a8e2602bd91
Serhat Turan <80906568+SerhatTurann@users.noreply.github.com>
2025-05-10 19:09:30 +0300

    updated HomeIssuePreview

--- ogretenler_odasi/src/components/HomeIssuePreview.jsx
+++ ogretenler_odasi/src/components/HomeIssuePreview.jsx
@@ -1,16 +1,45 @@
-// src/components/HomeIssuePreview.jsx
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import "../Css/HomeIssuePreview.css";
 
 const HomeIssuePreview = () => {
   const [issues, setIssues] = useState([]);
   const [hoveredId, setHoveredId] = useState(null);
+  const [visibleCount, setVisibleCount] = useState(6);
+
+  const updateVisibleCount = useCallback(() => {
+    const width = window.innerWidth;
+    if (width < 600) {
+      setVisibleCount(2);
+    } else if (width < 960) {
+      setVisibleCount(3);
+    }
+      else if (width < 1200) {
+      setVisibleCount(4);
+    } else if(width < 1500){
+      setVisibleCount(5);
+    }else{
+      setVisibleCount(6)
+    }
+  }, []);
+
+  useEffect(() => {
+    updateVisibleCount(); // ilk başta çalıştır
+    window.addEventListener("resize", updateVisibleCount);
+    return () => window.removeEventListener("resize", updateVisibleCount);
+  }, [updateVisibleCount]);
 
   useEffect(() => {
     fetch(`${import.meta.env.BASE_URL}data/issues.json`)
       .then((res) => res.json())
-      .then((data) => setIssues(data.slice(0, 6))) // sadece ilk 5 sayıyı al
+      .then((data) => {
+        const updatedData = data.map((issue) => ({
+          ...issue,
+          defaultImg: `${issue.defaultImg}`,
+          hoverImg: `${issue.hoverImg}`,
+        }));
+        setIssues(updatedData);
+      })
       .catch((err) => console.error("Veri alınamadı", err));
   }, []);
 
@@ -21,7 +50,7 @@ const HomeIssuePreview = () => {
         <div className="home-issue-line"></div>
       </div>
       <div className="home-issue-cards">
-        {issues.map((issue) => (
+        {issues.slice(0, visibleCount).map((issue) => (
           <Link
             to={`/issue/${issue.id}`}
             key={issue.id}
